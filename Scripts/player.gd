e659f809689455f683f53d4243fc8d801020f199
Tim Johansson <totimom@live.se>
2024-11-15 19:15:14 +0100

    Add first iteration player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
-const SPEED = 300.0
+
+
+const SPEED = 7500.0
+
 
 func _physics_process(delta: float) -> void:
-	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
-	var vertical_direction := Input.get_axis("ui_up", "ui_down")
 
-	self.velocity.x = horizontal_direction * SPEED
-	self.velocity.y = vertical_direction * SPEED
+	var hor_direction := Input.get_axis("ui_left", "ui_right")
+	var vert_direction := Input.get_axis("ui_up", "ui_down")
+	velocity = Vector2(hor_direction, vert_direction) * SPEED * delta
 
-	move_and_collide(self.velocity * delta)
+	move_and_slide()
