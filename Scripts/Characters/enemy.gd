0abf4b43b83739246d3d0fdb7d2e2bd0241b8360
Tim Johansson <totimom@live.se>
2024-11-16 08:14:32 +0100

    fix: Only start enemy cooldown if player was hit

--- Scripts/Characters/enemy.gd
+++ Scripts/Characters/enemy.gd
@@ -6,29 +6,34 @@ const SPEED = 100.0
 
 var dmg = 1.0
 
+var player_collided_cooldown_timer = 0.2
 var cooldown_timer: Timer
 var may_attack = true
 var dir: Vector2
 
-signal collided(damage: float)
+signal player_was_hit()
 
 func set_direction(dir: Vector2) -> void:
 	self.velocity = dir.normalized() * SPEED
 
 func _ready() -> void:
 	cooldown_timer = Timer.new()
+	cooldown_timer.autostart = false
 	self.add_child(cooldown_timer)
-	cooldown_timer.connect("timeout", func(): self.may_attack = true)
+	cooldown_timer.connect("timeout", self._timeout)
+	self.connect("player_was_hit", self._collided_with_player)
 
-func _physics_process(delta: float) -> void:
+func _collided_with_player() -> void:
+	self.may_attack = false
+	self.cooldown_timer.start(self.player_collided_cooldown_timer)
+
+func _timeout() -> void:
+	self.may_attack = true
+
+func _physics_process(_delta: float) -> void:
 	if move_and_slide():
 		var last_collision = get_last_slide_collision()
 		self.velocity = last_collision.get_normal() * SPEED
 
 		if self.may_attack:
-			last_collision.get_collider().emit_signal("enemy_collision", self.dmg)
-
-			# Use a cooldown timer on attacks to not overwhelm any player with
-			# damage in case they get stuck in a collision with an enemy
-			may_attack = false
-			cooldown_timer.start(0.2)
+			last_collision.get_collider().emit_signal("enemy_collision", self, self.dmg)
--- Scripts/Characters/player.gd
+++ Scripts/Characters/player.gd
@@ -1,20 +1,19 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 
-signal enemy_collision(damage: float)
+signal enemy_collision(enemy_node: Enemy, damage: float)
 
 func _ready():
 	self.connect("enemy_collision", _on_enemy_collision)
 
-func _physics_process(delta: float) -> void:
-
+func _physics_process(_delta: float) -> void:
 	var hor_direction := Input.get_axis("ui_left", "ui_right")
 	var vert_direction := Input.get_axis("ui_up", "ui_down")
 	velocity = Vector2(hor_direction, vert_direction) * SPEED
 
 	move_and_slide()
 
-func _on_enemy_collision(damage: float) -> void:
+func _on_enemy_collision(enemy_node: Enemy, damage: float) -> void:
 	print("Collided with enemy that made %f damage" %damage)
+	enemy_node.emit_signal("player_was_hit")
