aeafa685504f99645696232e898ce85980f8b573
Tim Johansson <totimom@live.se>
2024-11-16 09:57:27 +0100

    Move general item behavior to base class

--- Scripts/Items/Item_Base.gd
+++ Scripts/Items/Item_Base.gd
@@ -5,3 +5,7 @@ class_name Item_Base
 enum Food_Type {MOLDY_CREAM}
 
 var food_type: Food_Type
+
+func _process(_delta: float) -> void:
+	for body in self.get_overlapping_bodies():
+		body.emit_signal("item_pickup", self)
