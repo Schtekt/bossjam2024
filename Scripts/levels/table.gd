3e2755b13bcca1799d2f60a9c235b9cffb283b70
Tim Johansson <totimom@live.se>
2024-11-16 20:40:06 +0100

    fix: Ensure chair positions at table

--- Scripts/levels/table.gd
+++ Scripts/levels/table.gd
@@ -22,13 +22,14 @@ func randomize_chairs() -> void:
 	self.chairs_at_table = (randi() % 2) + 1
 	
 	var available_chair_placements = [Chair.Chair_Direction.LEFT, Chair.Chair_Direction.RIGHT]
-	
+
 	for i in range(self.chairs_at_table):
 		var chair_placement = randi() % available_chair_placements.size()
+		var chair = available_chair_placements[chair_placement]
 		available_chair_placements.erase(chair_placement)
-		if chair_placement == Chair.Chair_Direction.LEFT:
+		if chair == Chair.Chair_Direction.LEFT:
 			_create_right_chair()
-		elif chair_placement == Chair.Chair_Direction.RIGHT:
+		elif chair == Chair.Chair_Direction.RIGHT:
 			_create_left_chair()
 
 func _ready() -> void:
