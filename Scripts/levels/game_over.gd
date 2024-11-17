259d419630aedbefa4909fb6b87c277e6efd7abf
Tim Johansson <totimom@live.se>
2024-11-17 12:01:38 +0100

    Cleanup game over script

--- Scripts/levels/game_over.gd
+++ Scripts/levels/game_over.gd
@@ -4,12 +4,3 @@ class_name GameOver
 func set_score(score: int) -> void:
 	var score_label: Label = get_node("Score")
 	score_label.text = "Your Score: " + str(score)
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
